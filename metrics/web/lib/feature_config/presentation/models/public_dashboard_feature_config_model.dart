33c0b39f2cbff1df2d1e17ca49e3b3fbd274851b
Dmytro Tkachuk <dmytro.tkachuk@solid.software>
2021-08-12 14:58:57 +0300

    feat: Implements feature config module

--- metrics/web/lib/feature_config/presentation/models/public_dashboard_feature_config_model.dart
+++ metrics/web/lib/feature_config/presentation/models/public_dashboard_feature_config_model.dart
@@ -20,4 +20,30 @@ class PublicDashboardFeatureConfigModel extends Equatable {
   const PublicDashboardFeatureConfigModel({
     @required this.isEnabled,
   }) : assert(isEnabled != null);
+
+  /// Returns a new instance of the [PublicDashboardFeatureConfigModel] that is a combination
+  /// of this user profile model and the given [PublicDashboardFeatureConfigModel].
+  ///
+  /// If the given [PublicDashboardFeatureConfigModel] is `null`, returns this user profile model.
+  PublicDashboardFeatureConfigModel merge(
+      PublicDashboardFeatureConfigModel publicDashboardFeatureConfigModel) {
+    if (publicDashboardFeatureConfigModel == null) return this;
+
+    return copyWith(
+      isEnabled: publicDashboardFeatureConfigModel.isEnabled,
+    );
+  }
+
+  /// Creates the new instance of the [PublicDashboardFeatureConfigModel]
+  /// based on the current instance.
+  ///
+  /// If any of the passed parameters are `null`, or parameter isn't specified,
+  /// the value will be copied from the current instance.
+  PublicDashboardFeatureConfigModel copyWith({
+    bool isEnabled,
+  }) {
+    return PublicDashboardFeatureConfigModel(
+      isEnabled: isEnabled ?? this.isEnabled,
+    );
+  }
 }
